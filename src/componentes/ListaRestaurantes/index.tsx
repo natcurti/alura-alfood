8f545c272fec89d6f39b168f7ab684134325a084
natcurti <naticurti01@gmail.com>
2024-02-29 16:55:00 -0300

    Implement functionality to search a restaurant

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -2,20 +2,39 @@ import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { IPaginacao } from "../../interfaces/IPaginacao";
+import { Button, TextField } from "@mui/material";
+
+interface IParametrosBusca {
+  ordering?: string;
+  search?: string;
+}
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proxPagina, setProxPagina] = useState("");
   const [paginaAnterior, setPaginaAnterior] = useState("");
+  const [busca, setBusca] = useState("");
 
-  const carregarDados = (url: string) => {
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
     axios.get<IPaginacao<IRestaurante>>(url).then((response) => {
       setRestaurantes(response.data.results);
       setProxPagina(response.data.next);
       setPaginaAnterior(response.data.previous);
     });
+  };
+
+  const buscarRestaurante = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const opcoes = {
+      params: {} as IParametrosBusca,
+    };
+    if (busca) {
+      opcoes.params.search = busca;
+    }
+    carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
   };
 
   useEffect(() => {
@@ -27,6 +46,19 @@ const ListaRestaurantes = () => {
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
+      <form onSubmit={buscarRestaurante}>
+        <h2>Busque seu restaurante preferido:</h2>
+        <TextField
+          id="outlined-basic"
+          label="Restaurante"
+          variant="outlined"
+          value={busca}
+          onChange={(e) => setBusca(e.target.value)}
+        />
+        <Button variant="outlined" type="submit">
+          Buscar
+        </Button>
+      </form>
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
